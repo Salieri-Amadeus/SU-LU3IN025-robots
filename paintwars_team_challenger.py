02ee5980c0967fb93cfc4642e7f37becc3fe8bd8
Salieri <dison.vli2003@gmail.com>
2024-01-26 12:08:24 +0100

    tme1

--- paintwars_team_challenger.py
+++ paintwars_team_challenger.py
@@ -1,23 +1,43 @@
-# Projet "robotique" IA&Jeux 2021
-#
-# Binome:
-#  Prénom Nom: _________
-#  Prénom Nom: _________
-
 def get_team_name():
-    return "[ team with no name ]" # à compléter (comme vous voulez)
+    return "Splatoon"  # 团队名称
+
+def get_extended_sensors(sensors):
+    for key in sensors:
+        sensors[key]["distance_to_robot"] = 1.0
+        sensors[key]["distance_to_wall"] = 1.0
+        if sensors[key]["isRobot"]:
+            sensors[key]["distance_to_robot"] = sensors[key]["distance"]
+        else:
+            sensors[key]["distance_to_wall"] = sensors[key]["distance"]
+    return sensors
 
 def step(robotId, sensors):
+    sensors = get_extended_sensors(sensors)
 
-    translation = 1 # vitesse de translation (entre -1 et +1)
-    rotation = 0 # vitesse de rotation (entre -1 et +1)
+    translation = 1  # 默认前进速度
+    rotation = 0  # 默认不转向
 
-    if sensors["sensor_front_left"]["distance"] < 1 or sensors["sensor_front"]["distance"] < 1:
-        rotation = 0.5  # rotation vers la droite
-    elif sensors["sensor_front_right"]["distance"] < 1:
-        rotation = -0.5  # rotation vers la gauche
+    # 避免碰撞行为（最高优先级）
+    if (sensors["sensor_front_left"]["distance_to_wall"] < 1 and sensors["sensor_front_left"]["distance_to_wall"] > 0.5) or (sensors["sensor_front"]["distance_to_wall"] < 1 and sensors["sensor_front"]["distance_to_wall"] > 0.5):
+        rotation = 0.2 + robotId * 0.1  # 向右转
+    elif (sensors["sensor_front_right"]["distance_to_wall"] < 1 and sensors["sensor_front_right"]["distance_to_wall"] > 0.5):
+        rotation = -0.2 - robotId * 0.1  # 向左转
+    elif (sensors["sensor_front_left"]["distance_to_wall"] < 0.5) or (sensors["sensor_front"]["distance_to_wall"] < 0.5):
+        rotation = 0.5 + robotId * 0.1
+    elif (sensors["sensor_front_right"]["distance_to_wall"] < 0.5):
+        rotation = -0.5 - robotId * 0.1
+   
 
-    if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == False:
-        enemy_detected_by_front_sensor = True # exemple de détection d'un robot de l'équipe adversaire (ne sert à rien)
+    # 避免撞到盟友（次高优先级）
+    elif any(sensors[sensor]["isRobot"] and sensors[sensor]["isSameTeam"] for sensor in sensors):
+        rotation = 0.2 + robotId * 0.1
+
+    # 追踪敌人（最低优先级）
+    elif sensors["sensor_front"]["isRobot"] and not sensors["sensor_front"]["isSameTeam"]:
+        rotation = (1) * sensors["sensor_front_left"]["distance_to_robot"] - (1) * sensors["sensor_front_right"]["distance_to_robot"]
+    
+    # limite les valeurs de sortie entre -1 et +1
+    translation = max(-1,min(translation,1))
+    rotation = max(-1, min(rotation, 1))
 
     return translation, rotation
